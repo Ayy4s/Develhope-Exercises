44d68b48288d6fc78f5c44c88d860c99866d735a
Ibrahim Ozcan <112540536+Ayy4s@users.noreply.github.com>
2023-02-01 21:43:05 +0300

    Add files via upload

---
+++ Exercise Methods - 2.py
@@ -0,0 +1,17 @@
+num1 = 2.8
+num2 = 2.9
+num3 = 1j
+num4 = 3.5
+num5 = 4.8
+
+num1=(float(num1))
+num2=(int(num2))
+num3=(complex(num3))
+num4=(round(num4))
+num5=(round(num5))
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(num4))
+print(type(num5))
